0f2d0cb51aada0313c560785ad973dca601d210f
Daniel Cruz <dcruz.js@gmail.com>
2020-06-04 09:48:43 -0600

    Updates

---
+++ precache-manifest.d1104cff756e4e9bb93d8a2760e57257.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea03ced584806829f012f4820adcfab6",
+    "url": "/Jamming/index.html"
+  },
+  {
+    "revision": "acde4bca61bc49905b6f",
+    "url": "/Jamming/static/css/main.d1f0ae0a.chunk.css"
+  },
+  {
+    "revision": "d173f3af893ce4eb1ef2",
+    "url": "/Jamming/static/js/2.051a2115.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Jamming/static/js/2.051a2115.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acde4bca61bc49905b6f",
+    "url": "/Jamming/static/js/main.19640a71.chunk.js"
+  },
+  {
+    "revision": "9466d4092dec5e0aa929",
+    "url": "/Jamming/static/js/runtime-main.cd13da99.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/Jamming/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
